a122decff1791eea04c227bd72af6e657cb024be
BuildTools <carltondukeatx@gmail.com>
2022-05-29 17:48:17 -0500

    Added error catching for clan search

--- call_rest_api.py
+++ call_rest_api.py
@@ -2,12 +2,16 @@ import pip._vendor.requests
 import json
 
 def cleanTag(input):
-    output = input[1:len(input)]
-    output = "%23" + output
+    ouput = input
+    if input[0] == "#":
+        output = input[1:len(input)]
+        output = "%23" + input
+    else:
+        output = "%23" + input
     return output
 
 def driver(method, tag):
-    headers = {'Authorization': 'Bearer '}
+    headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFiNmE3MmUxLTQ4YzMtNGVhYy1iZmE5LThmNzVjNDdjMzY5MCIsImlhdCI6MTY1MzA4MTA3Mywic3ViIjoiZGV2ZWxvcGVyL2M0YTVjMGQ3LTk5ZTQtZWE3OS02M2U1LWFkZTFmYjE4YTIzYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk4LjYuMTE1LjE3NyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.yuyorgS5JFvQX0YdypUhJYbkUWh0C2uJ2u2EwpgnucfaSBLACeTrK7v3KFYye-yzqTy-HK6cJaSQMDxcvr8Ruw'}
     broad_endpoint = 'https://api.clashofclans.com/v1/'
     tag = cleanTag(tag)
     if method == "members" or method == "currentwar" or method == "warlog":
@@ -21,13 +25,7 @@ def driver(method, tag):
     elif method == "clans":
         signifier = 'clans/'
         this_endpoint = broad_endpoint + signifier + tag
-
     response = pip._vendor.requests.get(this_endpoint, headers=headers).text
     jsonOut = json.loads(response)
     
-    responseCode = 101
-    if 'reason' in jsonOut:
-        if jsonOut['reason'] == 'notFound':
-            responseCode = 404
-    
-    return jsonOut, responseCode
+    return jsonOut, pip._vendor.requests.get(this_endpoint, headers=headers).status_code
--- views.py
+++ views.py
@@ -9,7 +9,7 @@ def warlogInput():
     if request.method == 'POST':
         clan_tag = request.form['clan_tag']
         jsonOut, response = driver("clans", clan_tag)
-        if response != 404:
+        if response == 200:
             return render_template(
             "warlogInput.html",
             clan_found = "true",
@@ -18,8 +18,16 @@ def warlogInput():
             clan_level = jsonOut["clanLevel"],
             mem_count = jsonOut["members"]
         )
+        elif response == 400:
+            return render_template("warlogInput.html", error = "Incorrect parameters passed as request")
+        elif response == 403:
+            return render_template("warlogInput.html", error = "Access Denied. Check API token.")
+        elif response == 404:
+            return render_template("warlogInput.html", error = "Method param incorrect in 'call_rest_api(method, tag)'")
+        elif response == 503: 
+            return render_template("warlogInput.html", error = "Clash of Clans API under maintenence")
         else:
-            return render_template("warlogInput.html", error = "Not Found")
+            return render_template("warlogInput.html", error = "Unknown error")
 def warlogSearchClan():
     return render_template("warlog.html")
 def clan():
